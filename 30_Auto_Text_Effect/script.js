865f75ec8284df003161ddac2ad879f7a28e003a
satania-lee <rockky.tl@gmail.com>
2022-05-27 17:48:40 +0300

    d30

---
+++ 30_Auto_Text_Effect/script.js
@@ -0,0 +1,23 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const textInput = document.getElementById('textInput')
+let text = 'We Love Programming!'
+let i = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, i)
+
+    i++
+
+    if(i > text.length) {
+        i = 1
+    }
+
+    setTimeout(writeText,speed)
+}
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+textInput.addEventListener('input', (e) => text = e.target.value)
